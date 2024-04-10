1abf6c8a2f2903d5af39fc91be31d86dafe0d3d7
William Liburd <70923068+wl2352@users.noreply.github.com>
2024-04-10 14:50:55 -0400

    buildv2

--- Assets/Allan's Assets/Scripts/Enemies/Behavior/E_Attack.cs
+++ Assets/Allan's Assets/Scripts/Enemies/Behavior/E_Attack.cs
@@ -1,7 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Experimental.GraphView;
-using UnityEditor.PackageManager.Requests;
 using UnityEngine;
 
 public class E_Attack : MonoBehaviour
